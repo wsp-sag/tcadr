c32408d2718f75805a6bb5545b089c70a290f4c1
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-04-15 13:32:11 -0400

    Added better namespace control.

--- R/read_tcad.R
+++ R/read_tcad.R
@@ -32,7 +32,7 @@ read_tcad_bin <- function(dir, file_name){
   )
 
   # Read each attribute in DCB from binary file.
-  df <- tbl_df(as.data.frame(
+  df <- dplyr::tbl_df(as.data.frame(
     get_df_from_binary(bin_file, dcb$name, dcb$type, dcb$start, dcb$width,
                        row_length),
     stringsAsFactors = FALSE)
