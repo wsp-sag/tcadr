615a7f89771b9be3f6553db0379fda68cbf03614
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-04-15 11:05:29 -0400

    Open and close file stream

--- src/write_to_df.cpp
+++ src/write_to_df.cpp
@@ -5,7 +5,6 @@
 using namespace Rcpp;
 using namespace std;
 
-
 // [[Rcpp::export]]
 List get_df_from_binary(
     String bin_file,
@@ -18,8 +17,26 @@ List get_df_from_binary(
   List my_list(n_fields);
   my_list.attr("names") = name;
 
-  //Rcout << bin_file;
+  // Open the binary data file and make sure it exists
+  std::fstream bf;
+  bf.open(bin_file, ios::in | ios::binary | ios::ate);
+  if(!bf.is_open()){
+    throw std::range_error("could not open binary file");
+  }
 
+  int file_size = bf.tellg();
+  int n_rows = file_size / row_length;
+
+
+
+  //Rcout << bin_file;
+  Rcout << "row length" << row_length << endl;
+  Rcout << "number of fields" << n_fields << endl;
+  Rcout << "number rows" << n_rows << endl;
+  Rcout << "file size" << file_size << endl;
+
+  //Close file
+  bf.close();
 
   // change list to data.frame, and return
   return(my_list);
