7d87610125e8f52304bafa7d6e892dafc145dd8d
Greg Macfarlane <gregmacfarlane@gmail.com>
2015-09-24 16:45:53 -0400

    Only require a single filename.
    This allows the package to behave much more like other functions in R. Closes #3.

--- R/read_tcad.R
+++ R/read_tcad.R
@@ -2,8 +2,7 @@
 #'
 #' @description Read a TransCAD \code{.bin} binary data file into the workspace.
 #'
-#' @param dir The path to the directory where the files are located
-#' @param file_name The name of the binary file, e.g. \code{foo.bin}
+#' @param file The path and name of the binary data file, e.g. \code{data/foo.bin}
 #' @param df_only Should the function return a `data.frame` or a list with
 #' the descriptions appended? \code{default = TRUE}
 #'
@@ -12,13 +11,11 @@
 #' a list with two elements: \code{description} contains any data labels
 #' on the dictionary file; \code{df} is the data frame.
 #'
-read_tcad_bin <- function(dir, file_name, df_only = TRUE){
+read_tcad_bin <- function(file, df_only = TRUE){
 
 
-  # Get file strings for the bin file as well as the DCB file.
-  base_file_name <- gsub(".bin", "", file_name)
-  bin_file <- file.path(dir, file_name)
-  dcb_file <- file.path(dir, paste(base_file_name, "DCB", sep = "."))
+  # Get file string for the DCB file.
+  dcb_file <- gsub(".bin", ".DCB", file)
 
   # Read binary file attributes from DCB file
   row_length <- as.numeric(
@@ -30,7 +27,7 @@ read_tcad_bin <- function(dir, file_name, df_only = TRUE){
 
   # Read each attribute in DCB from binary file.
   df <- dplyr::tbl_df(as.data.frame(
-    get_df_from_binary(bin_file, dcb$name, dcb$type,
+    get_df_from_binary(file, dcb$name, dcb$type,
                        dcb$start, dcb$width,
                        row_length),
     stringsAsFactors = FALSE)
